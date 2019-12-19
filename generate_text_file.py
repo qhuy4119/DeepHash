0ebb258e5dafd65bcf488d62ffb68076e5118252
Quang Huy <quanghuy4119@gmail.com>
2019-12-19 22:28:22 +0700

    add command line argument for number of images to write to file

--- generate_text_file.py
+++ generate_text_file.py
@@ -2,6 +2,8 @@ import os
 from os import listdir
 from os.path import isfile, join
 import argparse
+import logging
+
 
 def count_num_classes(fileList):
     num_classes = 0
@@ -10,15 +12,20 @@ def count_num_classes(fileList):
             num_classes += 1
     return num_classes
 
-def write_file(filePath, fileList, num_classes, prefix):
+def write_file(filePath, fileList, num_classes, prefix, num, logFile='log.txt'):
+    logging.basicConfig(filename=logFile,level=logging.DEBUG, filemode='w')
     with open(filePath, 'w') as outFile:
         i = 0
         current_class_index = 0
-        while i < len(fileList):
+        logging.info('Current img: %d', i + 1)
+        while i < len(fileList): 
             outFile.write(os.path.join(prefix, fileList[i]) + " ")
             write_one_hot_encoder(current_class_index, outFile, num_classes)
             j = i + 1
             while j < len(fileList):
+                if num and j + 1 > num:
+                    return
+                logging.info('Current img: %d', j + 1)
                 if fileList[j].startswith(fileList[i]):
                     outFile.write(os.path.join(prefix, fileList[j]) + " ")
                     write_one_hot_encoder(current_class_index, outFile, num_classes)
@@ -45,8 +52,12 @@ if __name__ == "__main__":
     parser.add_argument('d', help='specify the directory of image')
     parser.add_argument('--prefix', help='specify the name of folder to prefix when writing to text file. Default is: data', default='data')
     parser.add_argument('-o', '--output', help="specify the path of output file, including file name", default='train.txt')
+    parser.add_argument('--log', help='specify directory of log file', default='log.txt')
+    parser.add_argument('--num', help='specify num of images', type=int)
     args = parser.parse_args()
     files = [f for f in listdir(args.d) if isfile(join(args.d, f))]
     files.sort()
+    if args.num:
+        files = files[:args.num]
     num_classes = count_num_classes(files)
-    write_file(args.output, files, num_classes, args.prefix)
+    write_file(args.output, files, num_classes, args.prefix, args.num, args.log)
