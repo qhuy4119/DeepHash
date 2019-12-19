e79051f5f2d13e80bd98271a5fc9fb2b80c7c1d7
Quang Huy <quanghuy4119@gmail.com>
2019-12-19 14:23:39 +0700

    convert image float to int before saving

--- generate_images.py
+++ generate_images.py
@@ -1,17 +1,18 @@
 import argparse
 import os
 from keras.preprocessing.image import ImageDataGenerator
-from PIL import Image
 import time
-import matplotlib.pyplot as plt
 import imageio
+from tqdm import trange
+import tensorflow as tf
+import numpy as np
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-i','--input', help='directory of input images')
     parser.add_argument('-o', '--output', help='directory of output images')
     parser.add_argument('--size', help='size of input and output images', nargs=2, type=int, default=[512, 512])
-    parser.add_argument('-b', '--batch', help="specify batch size", type=int, default=32)
+    parser.add_argument('-n', help='number of augmented images for each original image', type=int, default=1)
     args = parser.parse_args()
     IMG_SIZE = (args.size[0], args.size[1])
     image_generator = ImageDataGenerator(   rescale=(1/255),
@@ -25,14 +26,9 @@ if __name__ == "__main__":
                                         )
     image_iterator = image_generator.flow_from_directory(args.input, target_size=IMG_SIZE, batch_size=1, shuffle=None, \
                                                         class_mode='input', interpolation='bicubic')
-    for i in range(len(image_iterator.filenames)):
-        batchX, batchY = image_iterator.next()
-        
-        # plt.imshow(batchX[0])
-        # plt.show()
-        # image = Image.fromarray(batchX[0])
-        filename = os.path.split(image_iterator.filenames[i])[-1] + '_' + str(time.time()) +'.jpg'
-        imageio.imwrite(os.path.join(args.output, filename), batchX[0])
-        # image.save()
-        # print(img)
+    for i in range(args.n):
+        for i in trange(len(image_iterator.filenames)):
+            batchX, batchY = image_iterator.next()
+            filename = os.path.split(image_iterator.filenames[i])[-1] + '_' + str(time.time()) +'.jpg'
+            imageio.imwrite(os.path.join(args.output, filename), (batchX[0]*255).astype(np.uint8))
 
